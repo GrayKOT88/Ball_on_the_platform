ae1788927b97ecd0f1206302474ce77fa10e70d7
GrayKOT88 <skotkin@yandex.ru>
2025-03-06 23:00:32 +0300

    03.06.25

---
+++ Ball on the platform/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public class CameraRotation : MonoBehaviour
+    {
+        public float rotationSpeed;
+
+        private void Update()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
